116da54c2bed72ba27b1f3f1b7a57a92bb5a6b42
ifyfaith24 <ifyfaith24@gmail.com>
2023-02-22 06:33:21 -0800

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,39 +5,21 @@
 */
 void jack_bauer(void)
 {
-int h1;
-int h2;
-int m1;
-int m2;
-int a = 9;
-h2 = 0;
-while (h2 <= 2)
+int a = 0, b = 23, a2 = 0, b2 = 59;
+while (a <= b)
 {
-if (h2 == 2)
+while (a2 <= b2)
 {
-a = 3;
-}
-h1 = 0;
-while (h1 <= a)
-{
-m2 = 0;
-while (m2 <= 5)
-{
-m1 = 0;
-while (m1 <= 9)
-{
-_putchar('0' + h2);
-_putchar('0' + h1);
+_putchar(a < 9 ? 0 + '0' : a / 10 + '0');
+_putchar(a < 9 ? a + '0' : a % 10 + '0');
 _putchar(':');
-_putchar('0' + m2);
-_putchar('0' + m1);
+_putchar(a2 < 9 ? 0 + '0' : a2 / 10 + '0');
+_putchar(a2 < 9 ? 0 + '0' : a2 % 10 + '0');
 _putchar('\n');
-m1++;
+a2++;
 }
-m2++;
-}
-h1++;
-}
-h2++;
+a2 = 0;
+a++;
 }
 }
+
