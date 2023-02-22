00893fc2248a4bc92b822037377855a005d9b21e
ifyfaith24 <ifyfaith24@gmail.com>
2023-02-22 06:08:27 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,25 +7,36 @@
  */
 void print_to_98(int n)
 {
-if (n > 98)
+if (n <= 98)
 {
-while (n >= 98)
+for (; n <= 98; n++)
+{
+if (n == 98)
 {
 printf("%d", n);
-if (n != 98)
-printf(",");
-n--;
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
+}
 }
 }
 else
 {
-while (n <= 98)
+for (; n >= 98; n--)
+{
+if (n == 98)
 {
 printf("%d", n);
-if (n != 98)
-printf(",");
-n++;
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
 }
 }
-putchar('\n');
+}
 }
