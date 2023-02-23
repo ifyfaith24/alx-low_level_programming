f1452859d1d70959cb4d75806daa01bd2f9279f7
ifyfaith24 <ifyfaith24@gmail.com>
2023-02-23 08:38:14 -0800

    2-main.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - prints a test that multiply two int
+ * @a: The first int
+* @b: The second int
+ * Return: the result of multiplication of int a and b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
