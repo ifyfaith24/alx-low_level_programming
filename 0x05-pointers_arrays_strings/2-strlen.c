b7c1b973ef1906acd6c5f9277db13632c13c0a88
ifyfaith24 <ifyfaith24@gmail.com>
2023-02-27 09:05:21 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s: String
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+int longi = 0;
+
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+return (longi);
+}
